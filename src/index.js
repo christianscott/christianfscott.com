085c8e71f8a0996e01db6832d9d305975902ac32
chrfrasco <christianfscott@gmail.com>
2017-09-24 06:12:31 +1300

    Add crawling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-snapshot";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
+render(
   <Router>
     <App />
   </Router>,
