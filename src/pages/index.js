2821152645f6532f06a0bb86af126e2661e5fbf0
Christian Scott <christianfscott@gmail.com>
2019-03-11 22:17:39 +1100

    remove Hr

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
-import { Hr } from "../components/hr"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
